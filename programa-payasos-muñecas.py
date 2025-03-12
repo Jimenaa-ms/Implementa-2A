4bebaa21b7e1f0fb7c8cc95d4a6f8b567d0ea842
Jimenaa-ms <jimenamusico@gmail.com>
2025-03-12 11:57:27 -0600

    Add files via upload

---
+++ programa-payasos-muñecas.py
@@ -0,0 +1,4 @@
+payasos=int(input("Ingrese el numero de payasos vendidos"))
+muñecas=int(input("Ingrese el numero de muñecas vendidas"))
+peso=(payasos*112)+(muñecas*75)
+print("El peso total del paquete es de:", peso,"g")
